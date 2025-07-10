21d42a5debac456fff1aab0fbccdddf61233695c
Maximilien Nadji <maximilien.nadji@pm.me>
2025-07-10 19:23:36 +0000

    replace libvirt by incus (#61)

--- opentofu/local.tf
+++ opentofu/local.tf
@@ -1,3 +1,10 @@
 locals {
-  vms = var.vm_definitions
+  instances = var.instances
+  hashed_passwd = var.hashed_password != "" ? var.hashed_password : (
+    fileexists(var.password_file_path) ? file(var.password_file_path) : ""
+  )
+  resolved_password_path = var.password_file_path == ".passwd" ? "${path.module}/.passwd" : var.password_file_path
+  ssh_public_key = var.ssh_public_key_content != "" ? var.ssh_public_key_content : (
+    fileexists(local.resolved_password_path) ? file(local.resolved_password_path) : ""
+  )
 }
--- opentofu/outputs.tf
+++ opentofu/outputs.tf
@@ -2,9 +2,9 @@ output "cloud_init" {
   value = [for init in data.template_file.user_data : init.rendered]
 }
 
-output "vm_ipv4_addresses" {
-  description = "IPv4 addresses of all VMs"
+output "instances_ipv4_addresses" {
+  description = "IPv4 addresses of all instances"
   value = {
-    for name, vm in libvirt_domain.vms : name => vm.network_interface[0].addresses[0]
+    for name, instance in incus_instance.instances : name => instance.ipv4_address
   }
 }
