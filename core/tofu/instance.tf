967ef657956aafb14b9a3b54ead795b4b5e331cd
nadmax <maximilien.nadji@pm.me>
2025-08-28 18:01:48 +0200

    aaa

--- core/tofu/instance.tf
+++ core/tofu/instance.tf
@@ -1,12 +1,14 @@
 resource "docker_image" "debian_k3s" {
   name         = "rancher/k3s:v1.32.8-k3s1-amd64"
   keep_locally = false
+  force_remove = true
 }
 
 resource "docker_container" "container" {
   image   = docker_image.debian_k3s.image_id
   name    = "controlplane"
   command = ["server", "--disable=traefik"]
+  remove_volumes = true
 
   ports {
     internal = var.docker_internal_port
